97f6109ea2ecb3983a15ce7c86d7bfc96df53304
k175 <82032493+k175jp@users.noreply.github.com>
2023-02-13 14:48:32 +0900

    Add files via upload

---
+++ encrypt.py
@@ -0,0 +1,10 @@
+import pwn
+
+with open('flag.png', 'rb') as f:
+    flag = f.read()
+
+xor_key = 'xxxxxxxxxx' # I forgot about it... But it was less than 10 letters.
+crypted_flag = pwn.xor(flag, xor_key)
+
+with open('encrypted_flag.png', 'wb') as f:
+    f.write(crypted_flag)
