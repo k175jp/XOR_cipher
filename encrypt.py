085d70624367231c10c97706af31a525d797ce31
k175 <82032493+k175jp@users.noreply.github.com>
2023-02-13 14:52:50 +0900

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -4,7 +4,7 @@ with open('flag.png', 'rb') as f:
     flag = f.read()
 
 xor_key = 'xxxxxxxxxx' # I forgot about it... But it was less than 10 letters.
-crypted_flag = pwn.xor(flag, xor_key)
+encrypted_flag = pwn.xor(flag, xor_key)
 
 with open('flag.png', 'wb') as f:
-    f.write(crypted_flag)
+    f.write(encrypted_flag)
