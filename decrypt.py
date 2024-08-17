a50b9e2daa214c2915187685ee84b1689c7457b9
k175 <tai.sp.tai@gmail.com>
2024-08-17 16:55:43 +0900

    [Update] add comment

--- decrypt.py
+++ decrypt.py
@@ -1,18 +1,21 @@
 import pwn
 
-signature = b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A" # png signature
+# png signature
+signature = b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"
 
+# read xor flag
 with open("flag.png", "rb") as f:
     encrypt = f.read()
 
 # key = b'THISISXO'
-key = pwn.xor(encrypt, signature)[0:8]
+key = pwn.xor(encrypt, signature)[0:len(signature)]
 
-# key is less than 10 letters
+# key is less than 10 letters, so you guessed it.
 key = key + b"R"
 
-
+# decrypt flag
 flag = pwn.xor(encrypt, key)
 
+# write flag
 with open("decrypt_flag.png", "wb") as f:
     f.write(flag)
